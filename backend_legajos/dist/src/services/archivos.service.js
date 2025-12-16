5847b7b748d967295c2fd31a7d28a44b6100d803
Carlos Gonzales <cgonzalesmo@unsa.edu.pe>
2025-12-16 13:39:00 -0500

    subiendo cambios

--- backend_legajos/dist/src/services/archivos.service.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ArchivosService = void 0;
-const prisma_1 = require("../prisma");
-exports.ArchivosService = {
-    create: (data) => prisma_1.prisma.archivo.create({ data }),
-    delete: (id) => prisma_1.prisma.archivo.delete({ where: { id } })
-};
